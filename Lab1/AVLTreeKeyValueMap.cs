4fd8256a834100aaf6b153f9697521dac4e4278a
allisoncwalker <82103388+allisoncwalker@users.noreply.github.com>
2023-02-13 16:12:46 -0600

    created AVL and RedBlack

--- Lab1/AVLTreeKeyValueMap.cs
+++ Lab1/AVLTreeKeyValueMap.cs
@@ -1,10 +1,41 @@
-﻿namespace Lab1
+﻿using DSA.DataStructures.Trees;
+using Lab1;
+using System.Collections.Generic;
+
+namespace Lab1
 {
-    //TODO
-    class AVLTreeKeyValueMap<TKey, TValue>
+    class AVLTreeKeyValueMap<TKey, TValue> : IKeyValueMap<TKey, TValue>
     {
-        public AVLTreeKeyValueMap()
+        private AVLTreeMap<TKey, TValue> AVLTreeMap = new AVLTreeMap<TKey, TValue>();
+
+        public int Height => AVLTreeMap.Height;
+
+        public int Count => AVLTreeMap.Count;
+
+        public void Add(TKey key, TValue value)
         {
+            AVLTreeMap.Add(key, value);
         }
-    }
+
+        public KeyValuePair<TKey, TValue> Get(TKey key)
+        {
+            TValue value;
+            AVLTreeMap.TryGetValue(key, out value);
+            return new KeyValuePair<TKey, TValue>(key, value);
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (AVLTreeMap.ContainsKey(key))
+            {
+                AVLTreeMap.Remove(key);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
 }
--- Lab1/RedBlackTreeKeyValueMap.cs
+++ Lab1/RedBlackTreeKeyValueMap.cs
@@ -1,10 +1,39 @@
-﻿namespace Lab1
+﻿using System.Collections.Generic;
+using DSA.DataStructures.Trees;
+
+namespace Lab1
 {
-    //TODO
     public class RedBlackTreeKeyValueMap<TKey, TValue>
     {
-        public RedBlackTreeKeyValueMap()
+        private RedBlackTreeMap<TKey, TValue> RedBlackTreeMap = new RedBlackTreeMap<TKey, TValue>();
+
+        public int Height => RedBlackTreeMap.Height;
+
+        public int Count => RedBlackTreeMap.Count;
+
+        public void Add(TKey key, TValue value)
         {
+            RedBlackTreeMap.Add(key, value);
         }
+
+        public KeyValuePair<TKey, TValue> Get(TKey key)
+        {
+            TValue value;
+            RedBlackTreeMap.TryGetValue(key, out value);
+            return new KeyValuePair<TKey, TValue>(key, value);
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (RedBlackTreeMap.ContainsKey(key))
+            {
+                RedBlackTreeMap.Remove(key);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
     }
 }
+
